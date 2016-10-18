2a58b1d1ab11a9a02afbddd3a24b766c72cb796c
bgulbis <brian.gulbis@gmail.com>
2016-10-18 11:59:41 -0500

    create valid orders table

--- src/3-tidy_order_data.R
+++ src/3-tidy_order_data.R
@@ -1,11 +1,29 @@
 # tidy order data
 
 library(tidyverse)
+library(stringr)
+library(lubridate)
 library(edwr)
-
-ords <- read_data("data/raw", "orders") %>%
-    as.order_by()
 
 details <- read_data("data/raw", "details") %>%
     as.order_info()
 
+draw_times <- details %>%
+    filter(detail.descr == "Requested Start Date/Time") %>%
+    distinct(pie.id, order.id, detail.datetime)
+
+ords <- read_data("data/raw", "orders") %>%
+    as.order_by() %>%
+    left_join(draw_times, by = c("pie.id", "order.id"))
+
+order_times <- ords %>%
+    select(pie.id, order.id, order, detail.datetime, order.status, action.datetime) %>%
+    distinct(pie.id, order.id, order, detail.datetime, order.status, .keep_all = TRUE) %>%
+    spread(order.status, action.datetime)
+
+orders_valid <- order_times %>%
+    filter(is.na(Canceled),
+           is.na(Discontinued)) %>%
+    select(-Canceled, -Discontinued) %>%
+    mutate(request = str_detect(order, "Request"),
+           collect_diff = difftime(Collected, detail.datetime, units = "mins"))
