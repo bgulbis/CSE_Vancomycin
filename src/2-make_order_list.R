0b428d6a6071773b68aea8945b36512a14c2ac39
bgulbis <brian.gulbis@gmail.com>
2016-10-28 11:51:46 -0500

    combine levels and order timing for complete order.id list

--- src/2-make_order_list.R
+++ src/2-make_order_list.R
@@ -9,10 +9,20 @@ timing <- read_data("data/raw", "timing") %>%
     as.order_timing()
     # filter(order.unit %in% hvi)
 
-id <- concat_encounters(unique(timing$order.id), 950)
+vanc_levels <- read_data("data/raw", "vanc_level") %>%
+    as.labs() %>%
+    rename(order.id = `Clinical Event Order ID`,
+           event.unit = `Nurse Unit of Clinical Event`) %>%
+    filter(!is.na(event.unit))
+
+orders <- bind_rows(timing["order.id"], vanc_levels["order.id"]) %>%
+    distinct()
+
+id <- concat_encounters(orders$order.id, 950)
 
 # run EDW queries:
 #   * Orders - Actions - Source Order ID Prompt
 #   * Orders - Details - Source Order ID Prompt
 
 saveRDS(timing, "data/tidy/order_timing.Rds")
+saveRDS(vanc_levels, "data/tidy/vanc_levels.Rds")
